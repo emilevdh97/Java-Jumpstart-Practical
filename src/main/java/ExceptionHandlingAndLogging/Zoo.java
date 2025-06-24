da589bc702fc458e0fe67761b333d9b26d32a0f1
Sizwe <sizwetshabangu97@gmail.com>
2025-06-24 17:49:26 +0200

    Updating the start point for the practical

--- src/main/java/ExceptionHandlingAndLogging/Zoo.java
+++ src/main/java/ExceptionHandlingAndLogging/Zoo.java
@@ -8,15 +8,14 @@ public class Zoo {
     private static final File file = new File("src/wrong/folder/animals.csv");
 
     public static void main(String[] args) {
-        List<Animal> animals = new ArrayList<>(readAnimals());
+        List<Animal> animals;
+        animals = new ArrayList<>(readAnimals());
         for (Animal animal : animals) {
-            System.out.println("Animal {}" + animal);
+
         }
     }
 
     private static List<Animal> readAnimals() {
-        /*FileReader fileReader = new FileReader(file);
-        return animalsReader(fileReader);*/
         return List.of();
     }
 }
